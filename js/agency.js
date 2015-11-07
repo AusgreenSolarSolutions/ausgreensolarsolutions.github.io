b63a943a43f446485a12bd2bcf23b7dd505bbd68
Muhammad Fawwaz <escaffe@gmail.com>
2015-11-07 19:37:46 +0800

    Change back to original bff site

--- js/agency.js
+++ js/agency.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
@@ -25,13 +25,9 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
+// Closes the Responsive Menu on Click outside Menu
+$('body > *').not('nav').click(function() {
+    if(!$('button.navbar-toggle').hasClass('collapsed')) {
+        $('.navbar-toggle:visible').click();
+    }
 });
